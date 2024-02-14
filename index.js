f35600e26395d2f95828bc3614ec98d196010300
PepsiDancer <160023114+PepsiDancer@users.noreply.github.com>
2024-02-14 14:34:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Limbia Studios"];
+const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
 
 
 let currentIndex = 0;
